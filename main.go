459b7e519a0eb61d610e8eabdb95962a471d9f59
Frank Kloeker <eumel@arcor.de>
2025-11-22 12:48:20 +0100

    fix import

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	//"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/aws/aws-sdk-go-v2/config"
 )
 
