09c9a242e5e4a1ab2f844bf564ba7c119ba2520b
Frank Kloeker <frank.kloeker@telekom.de>
2025-11-22 13:30:07 +0100

    add debug and full recursive search

--- main.go
+++ main.go
@@ -29,6 +29,8 @@ func main() {
 		region = "eu-central-1"
 	}
 
+	fmt.Println("=== Starte Verbindung zu S3 ===")
+
 	cfg, err := config.LoadDefaultConfig(context.Background(),
 		config.WithRegion(region),
 		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKey, secretKey, "")),
@@ -47,26 +49,8 @@ func main() {
 	// Regex für Mimir Block-IDs: 12+ alphanumerische Zeichen
 	blockRegex := regexp.MustCompile(`^[0-9A-Za-z]{12,}/$`)
 
-	// Alle Block-Ordner per CommonPrefixes auflisten
-	paginator := s3.NewListObjectsV2Paginator(client, &s3.ListObjectsV2Input{
-		Bucket:    aws.String(bucket),
-		Prefix:    aws.String(""),
-		Delimiter: aws.String("/"), // liefert Unterordner
-	})
-
-	var blocks []string
-	for paginator.HasMorePages() {
-		page, err := paginator.NextPage(ctx)
-		if err != nil {
-			log.Fatalf("Fehler beim Listen von S3: %v", err)
-		}
-		for _, cp := range page.CommonPrefixes {
-			dir := *cp.Prefix
-			if blockRegex.MatchString(dir) {
-				blocks = append(blocks, dir)
-			}
-		}
-	}
+	fmt.Println("=== Suche rekursiv nach Blöcken ===")
+	blocks := findBlocks(ctx, client, bucket, "", blockRegex)
 
 	fmt.Printf("Gefundene echte Blöcke: %d\n", len(blocks))
 	if len(blocks) == 0 {
@@ -88,16 +72,16 @@ func main() {
 			indexHeaderKey := block + "index-header"
 			newKey := block + "index-header.old"
 
-			// Prüfen, ob index-header existiert
+			fmt.Printf("Prüfe index-header für Block %s\n", block)
 			_, err := client.HeadObject(ctx, &s3.HeadObjectInput{
 				Bucket: aws.String(bucket),
 				Key:    aws.String(indexHeaderKey),
 			})
 			if err != nil {
-				// Kein Header vorhanden, trotzdem Block markieren
-				fmt.Printf("Block %s hat noch keinen index-header, wird trotzdem vom Compactor verarbeitet\n", block)
+				fmt.Printf("Block %s hat noch keinen index-header, wird trotzdem verarbeitet\n", block)
 			} else {
-				// Retry-Funktion
+				fmt.Printf("Block %s hat index-header, verschiebe nach .old\n", block)
+
 				retry := func(f func() error) error {
 					for i := 0; i < 3; i++ {
 						if err := f(); err != nil {
@@ -110,8 +94,9 @@ func main() {
 					return fmt.Errorf("Operation nach 3 Versuchen fehlgeschlagen")
 				}
 
-				// Copy index-header
+				// Copy
 				err = retry(func() error {
+					fmt.Printf("CopyObject: %s -> %s\n", indexHeaderKey, newKey)
 					_, err := client.CopyObject(ctx, &s3.CopyObjectInput{
 						Bucket:     aws.String(bucket),
 						CopySource: aws.String(bucket + "/" + indexHeaderKey),
@@ -122,7 +107,9 @@ func main() {
 				if err != nil {
 					fmt.Printf("WARN: Konnte %s nicht kopieren: %v\n", indexHeaderKey, err)
 				} else {
+					// Delete
 					_ = retry(func() error {
+						fmt.Printf("DeleteObject: %s\n", indexHeaderKey)
 						_, err := client.DeleteObject(ctx, &s3.DeleteObjectInput{
 							Bucket: aws.String(bucket),
 							Key:    aws.String(indexHeaderKey),
@@ -138,5 +125,43 @@ func main() {
 
 	wg.Wait()
 	fmt.Println("Fertig! Jetzt den Compactor starten, um neue Sparse Index-Header zu erzeugen.")
+}
+
+// findBlocks durchsucht rekursiv alle Unterordner nach Block-Ordnern
+func findBlocks(ctx context.Context, client *s3.Client, bucket string, prefix string, blockRegex *regexp.Regexp) []string {
+	var blocks []string
+	var nextPrefixes []string
+
+	maxKeys := int32(1000)
+	paginator := s3.NewListObjectsV2Paginator(client, &s3.ListObjectsV2Input{
+		Bucket:    aws.String(bucket),
+		Prefix:    aws.String(prefix),
+		Delimiter: aws.String("/"),
+		MaxKeys:   &maxKeys,
+	})
+
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			log.Fatalf("Fehler beim Listen von S3 unter Prefix '%s': %v", prefix, err)
+		}
+
+		for _, cp := range page.CommonPrefixes {
+			dir := *cp.Prefix
+			if blockRegex.MatchString(dir) {
+				fmt.Printf("Erkannter Block: %s\n", dir)
+				blocks = append(blocks, dir)
+			} else {
+				fmt.Printf("Rekursiver Scan in Unterordner: %s\n", dir)
+				nextPrefixes = append(nextPrefixes, dir)
+			}
+		}
+	}
+
+	for _, p := range nextPrefixes {
+		blocks = append(blocks, findBlocks(ctx, client, bucket, p, blockRegex)...)
+	}
+
+	return blocks
 }
 
